dc9f06e9c924f156f9bf6b0b2c482d0a5f3b7143
Dmytro Korduban <dkorduban.ua@gmail.com>
2026-04-14 00:49:39 -0700

    Export probe evaluation helpers

--- python/tsrl/policies/__init__.py
+++ python/tsrl/policies/__init__.py
@@ -1,6 +1,11 @@
 """Policy helpers for Twilight Struggle rollouts."""
 
-from tsrl.policies.jsd_probe import ProbeEvaluator
+from tsrl.policies.jsd_probe import (
+    ProbeEvaluator,
+    build_probe_set,
+    compute_jsd,
+    load_probe_model,
+)
 from tsrl.policies.minimal_hybrid import (
     DEFAULT_MINIMAL_HYBRID_PARAMS,
     MinimalHybridParams,
@@ -12,6 +17,9 @@ __all__ = [
     "DEFAULT_MINIMAL_HYBRID_PARAMS",
     "MinimalHybridParams",
     "ProbeEvaluator",
+    "build_probe_set",
     "choose_minimal_hybrid",
+    "compute_jsd",
+    "load_probe_model",
     "make_minimal_hybrid_policy",
 ]
