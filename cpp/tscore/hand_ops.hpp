0b1411eba24a97ea799691a02856e9271a49aaf9
Dmytro Korduban <dkorduban.ua@gmail.com>
2026-04-18 15:47:50 -0700

    WIP: add frame log hand ops signatures

--- cpp/tscore/hand_ops.hpp
+++ cpp/tscore/hand_ops.hpp
@@ -4,7 +4,9 @@
 
 #include <optional>
 #include <tuple>
+#include <vector>
 
+#include "decision_frame.hpp"
 #include "game_state.hpp"
 #include "policy_callback.hpp"
 
@@ -15,21 +17,24 @@ std::tuple<PublicState, bool, std::optional<Side>> apply_action_with_hands(
     const ActionEncoding& action,
     Side side,
     Pcg64Rng& rng,
-    const PolicyCallbackFn* policy_cb = nullptr
+    const PolicyCallbackFn* policy_cb = nullptr,
+    std::vector<DecisionFrame>* frame_log = nullptr
 );
 
 std::optional<std::tuple<PublicState, bool, std::optional<Side>>> resolve_trap_ar(
     GameState& gs,
     Side side,
     Pcg64Rng& rng,
-    const PolicyCallbackFn* policy_cb = nullptr
+    const PolicyCallbackFn* policy_cb = nullptr,
+    std::vector<DecisionFrame>* frame_log = nullptr
 );
 
 std::optional<std::tuple<PublicState, bool, std::optional<Side>>> resolve_cuban_missile_crisis_cancel(
     GameState& gs,
     Side side,
     Pcg64Rng& rng,
-    const PolicyCallbackFn* policy_cb = nullptr
+    const PolicyCallbackFn* policy_cb = nullptr,
+    std::vector<DecisionFrame>* frame_log = nullptr
 );
 
 }  // namespace ts
